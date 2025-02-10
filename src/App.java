c7bc2ce93dd673552be0fc127fdf24c5400fc203
Lenin27Amangandi <lenin.amangandi@epn.edu.ec>
2025-02-10 03:00:21 -0500

    Listo

--- src/App.java
+++ src/App.java
@@ -8,22 +8,16 @@ public class App {
 
         try {
             System.out.println("Haz iniciado la App de ArtVision!! :)");
-
             JFrame frame = new JFrame("ArtVision");
-
             ImageIcon icono = new ImageIcon(App.class.getResource("/utils/icons/museo-britanico.png"));
             if (icono.getImage() == null) {
                 throw new NullPointerException("No se encontró el icono.");
             }
             frame.setIconImage(icono.getImage());
-
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
             AppMuseoUI app = new AppMuseoUI(frame);
             app.iniciarApp();
-
             frame.setVisible(true);
-
         } catch (Exception e) {
             RAConfig.showMsgError("Ocurrió un error al iniciar la app: " + e.getMessage());
         }
