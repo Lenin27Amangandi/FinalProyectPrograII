bb58500bfc24cb587cf73fd3b669eeba5bec465c
AnndyR <anndy.rengifo@epn.edu.ec>
2025-02-09 21:42:19 -0500

    Configuracion del icono

--- src/App.java
+++ src/App.java
@@ -1,4 +1,3 @@
-import java.awt.HeadlessException;
 import javax.swing.*;
 
 import Framework.RAConfig;
@@ -11,14 +10,22 @@ public class App {
             System.out.println("Haz iniciado la App de ArtVision!! :)");
 
             JFrame frame = new JFrame("ArtVision");
+
+            ImageIcon icono = new ImageIcon(App.class.getResource("/utils/icons/museo-britanico.png"));
+            if (icono.getImage() == null) {
+                throw new NullPointerException("No se encontró el icono.");
+            }
+            frame.setIconImage(icono.getImage());
+
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
             AppMuseoUI app = new AppMuseoUI(frame);
             app.iniciarApp();
-            
-        } catch (HeadlessException e) {
+
+            frame.setVisible(true);
+
+        } catch (Exception e) {
             RAConfig.showMsgError("Ocurrió un error al iniciar la app: " + e.getMessage());
         }
-
     }
 }
